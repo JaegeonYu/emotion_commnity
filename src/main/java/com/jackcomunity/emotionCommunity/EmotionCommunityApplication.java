197a76adee38f98d8d30f99ca6d7c9309e36b3e8
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-26 15:14:15 +0900

    build: create Project and remote GitHub

---
+++ src/main/java/com/jackcomunity/emotionCommunity/EmotionCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.jackcomunity.emotionCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackcomunity/emotionCommunity/EmotionCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackcomunity.emotionCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
