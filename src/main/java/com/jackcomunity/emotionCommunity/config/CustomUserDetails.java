69776481be6b7311049038076cd9a117fca1aa61
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-01-08 16:52:44 +0900

    refactor: UserDetail Authority 타입 변경

--- src/main/java/com/jackcomunity/emotionCommunity/config/CustomUserDetails.java
+++ src/main/java/com/jackcomunity/emotionCommunity/config/CustomUserDetails.java
@@ -4,6 +4,8 @@ import com.jackcomunity.emotionCommunity.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
@@ -15,8 +17,10 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collectors = new ArrayList<>();
-        collectors.add(() -> user.getRole().getAuth());
+        String[] arr = new String[]{ user.getRole().getAuth()};
+        Collection<GrantedAuthority> collectors = AuthorityUtils.createAuthorityList(arr);
+        //collectors.add(() -> user.getRole().getAuth());
+
         return collectors;
     }
 
