a6549779f2dbd0213943a20e79600efec5308c74
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-02-23 15:50:18 +0900

    Feat: CommentServiceTest edit method 작성

--- src/test/java/com/jackcomunity/emotionCommunity/service/CommentServiceTest.java
+++ src/test/java/com/jackcomunity/emotionCommunity/service/CommentServiceTest.java
@@ -1,11 +1,14 @@
 package com.jackcomunity.emotionCommunity.service;
 
+import com.jackcomunity.emotionCommunity.entity.Comment;
 import com.jackcomunity.emotionCommunity.entity.Post;
 import com.jackcomunity.emotionCommunity.entity.User;
 import com.jackcomunity.emotionCommunity.repository.CommentRepository;
 import com.jackcomunity.emotionCommunity.repository.PostRepository;
 import com.jackcomunity.emotionCommunity.repository.UserRepository;
 import com.jackcomunity.emotionCommunity.request.CommentCreate;
+import com.jackcomunity.emotionCommunity.request.CommentEdit;
+import com.jackcomunity.emotionCommunity.util.Emotion;
 import com.jackcomunity.emotionCommunity.util.Roles;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -35,6 +38,7 @@ class CommentServiceTest {
 
     private User user;
     private Post post;
+    private Comment comment;
 
     @Test
     public void save_test(){
@@ -46,6 +50,18 @@ class CommentServiceTest {
         Assertions.assertEquals(commentTest.getContent(),commentRepository.findAll().get(0).getContent());
     }
 
+    @Test
+    public void edit_test(){
+        saveUserToRepository();
+        savePostToRepository();
+        saveCommentToRepository();
+        CommentEdit commentEdit = CommentEdit.builder().content("modify").build();
+        commentService.edit(commentEdit, comment.getId());
+
+        Comment findComment = commentRepository.findById(comment.getId()).get();
+        Assertions.assertEquals(commentEdit.getContent(), findComment.getContent());
+    }
+
     private void savePostToRepository() {
         post = postRepository.save(Post.builder().title("title").content("content").user(user).build());
     }
@@ -54,6 +70,10 @@ class CommentServiceTest {
         user = userRepository.save(User.builder().username("jack").password(passwordEncoder.encode("1234"))
                 .role(Roles.USER)
                 .email("yjk98053@gmail.com").nickname("biobebe").build());
+    }
+
+    private void saveCommentToRepository(){
+        comment = commentRepository.save(Comment.builder().content("test").emotion(Emotion.NEUTRAL).post(post).user(user).build());
     }
 
 }
